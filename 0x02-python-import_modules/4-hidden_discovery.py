c2563f22cc780871b39a1459a9d88e197de60545
Gladys-m <gladysmitambo@gmail.com>
2022-08-25 11:28:32 -0700

    using dir() function to read names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for i in names:
-        if names[:2] != "__":
-            print(i)
+    for counter in names:
+        if counter[:2] != "__":
+            print(counter)
