c52138da88a10d3838d66029efbe47ec6b968413
Gladys-m <gladysmitambo@gmail.com>
2023-04-11 22:02:06 +0300

    if and loops in JS

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const num = Number(arg);
+const line = 'C is fun';
+
+if (!num) {
+  console.log("Missing number of occurrences");
+} else {
+  for ( let i = 0; i < num; i++)
+    console.log(line);
+}
