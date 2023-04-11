1a7bc2459db5f64549874dc47a7ac40445e2ac1c
Gladys-m <gladysmitambo@gmail.com>
2023-04-11 22:05:10 +0300

    if statements and loops in JS

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ const num = Number(arg);
 const line = 'C is fun';
 
 if (!num) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
-  for ( let i = 0; i < num; i++)
+  for (let i = 0; i < num; i++) {
     console.log(line);
 }
