a36f21553f7147c776a748cfeb4760a18b0e5ed5
Gladys-m <gladysmitambo@gmail.com>
2023-04-16 00:59:19 +0300

    object oriented programming

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,20 +5,18 @@ import MySQLdb
 import sys
 
 if __name__ = "__main__":
-	db = MySQLdb.connect(
-		host="localhost",
-		user=sys.argv[1],
-		passwd=sys.argv[2],
-		db=sys.argv[3]
-	)
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
-	mycursor = db.cursor()
-	query = "SELECT * FROM states ORDER BY states.id ASC"
-	mycursor.execute(query)
-	rows = mycursor.fetchall()
+    mycursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    mycursor.execute(query)
+    rows = mycursor.fetchall()
 
-	for row in rows:
-		print(row)
+    for row in rows:
+        print(row)
 
-	mycursor.close()
-	db.close()
+    mycursor.close()
+    db.close()
