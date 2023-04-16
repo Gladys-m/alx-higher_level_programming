aec99ee2b71dbadd20a3fdd550a92ade659fc5bd
Gladys-m <gladysmitambo@gmail.com>
2023-04-16 17:58:18 +0300

    filter state by argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all states from database"""
+"""script that lists all states by argument"""
 
 import MySQLdb
 import sys
@@ -9,10 +9,11 @@ if __name__ == "__main__":
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
- 
+
     state_name = sys.argv[4]
     mycursor = db.cursor()
-    query = "SELECT states.id, name FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(state_name)
+    query = """SELECT states.id, name FROM states WHERE name LIKE BINARY '{}'
+             ORDER BY states.id ASC""".format(state_name)
     mycursor.execute(query)
     rows = mycursor.fetchall()
 
