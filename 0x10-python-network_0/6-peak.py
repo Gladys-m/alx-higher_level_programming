ca245634c40b56b2132a20a5dcca68a4bcd53b5a
Gladys-m <gladysmitambo@gmail.com>
2023-04-28 01:06:52 +0300

    the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function to find a peak in list """
 
+
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers == []:
