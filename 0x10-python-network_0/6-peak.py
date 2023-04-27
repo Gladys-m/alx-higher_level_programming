1f45bafab89f5590b647436e5a24e8f033af8fa3
Gladys-m <gladysmitambo@gmail.com>
2023-04-28 00:24:26 +0300

    finding peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """ Function to find a peak in list """
 
+
 def find_peak(list_of_integers):
     """ Finds a peak in a list of an unsorted array """
 
-    if not list_of_integers:
+    list_n = list_of_integers
+
+    if not list_n:
         return None
 
-    middle_index = len(list_of_integers) // 2
-    middle_element = list_of_integers[middle_index]
+    mid_index = len(list_n) // 2
+    mid_element = list_n[mid_index]
 
-    if (middle_index == 0 or middle_element >= list_of_integers[middle_index-1]) and \
-            (middle_index == len(list_of_integers)-1 or middle_element >= list_of_integers[middle_index+1]):
-        return middle_element
+    if (mid_index == 0 or mid_element >= list_n[mid_index-1]) and \
+            (mid_index == len(list_n)-1 or mid_element >= list_n[mid_index+1]):
+        return mid_element
 
-    if middle_index > 0 and list_of_integers[middle_index-1] > middle_element:
-        return find_peak(list_of_integers[:middle_index])
+    if mid_index > 0 and list_n[mid_index-1] > mid_element:
+        return find_peak(list_n[:mid_index])
     else:
-        return find_peak(list_of_integers[middle_index+1:])
-
+        return find_peak(list_n[mid_index+1:])
